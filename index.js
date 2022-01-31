5930a48d8e86deacdc1a4d2ef016de8a33046508
Ranjan.m <modranjan1999.com>
2022-01-31 16:03:49 +0530

    done from myside

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let screen = document.querySelector('#screen');
 let button = document.querySelectorAll('.display');
-let arr = [];
 
 let i = 0;
 document.addEventListener('keypress', (event) => {
@@ -16,75 +15,160 @@ for (item of button) {
         if (btntext == 'X') {
             btntext = '*';
         }
-        if (btntext == '%') {
-            btntext = '/';
-        }
         if (btntext == 'mod') {
 
             btntext = '%';
         }
-
         screen.value += btntext;
     })
 }
 // function to find exponential X^y
 function exponential() {
-    if(screen.value==="" || screen.value=="0"){
-        screen.value="0"
+    if (screen.value === "" || screen.value == "0") {
+        return screen.value = "0"
     }
-    else{
+    else {
         btntext = '**';
         screen.value += btntext;
     }
 }
-
+// function to find radian value
+function rad() {
+    screen.value = ((screen.value) * (Math.PI / 180));
+}
+// function to find degree value
+function deg() {
+    screen.value = (screen.value * 180 / Math.PI);
+}
+// trigonometric functions
 function sin() {
-    screen.value = Math.sin(screen.value)
-    arr.push(screen.value);
+    let check = document.getElementsByName("options")
+    console.log(check)
+    if (check[0].checked == false) {
+        screen.value = Math.sin(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.sin(value)
+    }
 }
-
 function cos() {
-    screen.value = Math.cos(screen.value)
-    arr.push(screen.value);
+    let check = document.getElementsByName("options")
+    if (check[0].checked == false) {
+        screen.value = Math.cos(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.cos(value)
+    }
 }
-
 function tan() {
-    screen.value = Math.tan(screen.value)
-    arr.push(screen.value);
+    let check = document.getElementsByName("options")
+    if (check[0].checked == false) {
+        screen.value = Math.tan(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.tan(value)
+    }
 }
+function csc() {
+    let check = document.getElementsByName("options")
+    if (check[0].checked == false) {
+        screen.value = Math.csc(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.csc(value)
+    }
+}
+function sec() {
+    let check = document.getElementsByName("options")
+    if (check[0].checked == false) {
+        screen.value = Math.sec(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.sec(value)
+    }
 
+}
+function cot() {
+    let check = document.getElementsByName("options")
+    if (check[0].checked == false) {
+        screen.value = Math.cot(screen.value)
+    }
+    else if (check[1].checked == false) {
+        let value = ((screen.value) * (Math.PI / 180));
+        screen.value = Math.cot(value)
+    }
+
+}
+// function to find ceil_value
+function ceil_value() {
+    screen.value = Math.ceil(screen.value)
+}
+// function to find floor_value
+function floor_value() {
+    screen.value = Math.floor(screen.value)
+}
+// function to find square/cube
 function pow() {
-    screen.value = Math.pow(screen.value, 2)
-    arr.push(screen.value);
+    let check = document.getElementById("square")
+    if (check.innerHTML == "x<sup>3</sup>") {
+        screen.value = Math.pow(screen.value, 3)
+    } else {
+        screen.value = Math.pow(screen.value, 2)
+    }
 }
-// function to find square-root
+// function to find square-root/cube-root
 function sqrt() {
-    screen.value = Math.sqrt(screen.value)
-    arr.push(screen.value);
-}
-//  function to find log
-function log() {
-    if (screen.value === "") {
-        screen.value = "invalid input"
-    }
-    else {
-        screen.value = Math.log(screen.value)
-        arr.push(screen.value);
+    let check = document.getElementById("sqrt")
+    if (check.innerHTML == "&#x221B;x") {
+        screen.value = Math.cbrt(screen.value)
+    } else {
+        screen.value = Math.cbrt(screen.value)
     }
 }
-//  function to find log-base-10
+// function to find 10 of any power
+function powerten() {
+    let check = document.getElementById("power10")
+    if (check.innerHTML == "2<sup>x</sup>") {
+        screen.value = Math.pow(2, screen.value)
+    } else {
+        screen.value = Math.pow(10, screen.value)
+    }
+}
+//  function to find log-base-10/log-base-Y X
 function logb10() {
+    let check = document.getElementById("log10")
     if (screen.value === "") {
         screen.value = "invalid input"
+    }
+    else if (check.innerHTML == "log<sub>y</sub> X") {
+        screen.value = Math.log10(screen.value);    // Math.log(y) / Math.log(x);
     }
     else {
         screen.value = Math.log10(screen.value)
-        arr.push(screen.value);
+    }
+}
+//  function to find log
+function log() {
+    let check = document.getElementById("log10")
+    if (screen.value === "") {
+        screen.value = "invalid input"
+    }
+    else if (check.innerHTML == "log<sub>y</sub> X") {
+        screen.value = Math.exp(screen.value)
+    }
+    else {
+        screen.value = Math.log(screen.value)
     }
 }
 
+
 function pi() {
-    screen.value = eval(22/7)
+    screen.value = Math.PI
 }
 
 function e() {
@@ -99,8 +183,6 @@ function factorial() {
         num -= 1
     }
     screen.value = tot
-    arr.push(screen.value);
-
 }
 // function to delete last digit
 function backspace() {
@@ -108,68 +190,85 @@ function backspace() {
 }
 // function to find inverse
 function inverse() {
-    if(screen.value=="0"){
-        screen.value="Cannot divide by 0"
+    if (screen.value == "0") {
+        screen.value = "Cannot divide by 0"
     }
-    else if(screen.value===""){
-        screen.value="0"
+    else if (screen.value === "") {
+        screen.value = "0"
     }
-    else{
+    else {
         screen.value = 1 / (screen.value)
-        arr.push(screen.value);
     }
 }
 // function to find absolute value
 function absolute() {
-    screen.value=Math.abs(screen.value)
+    screen.value = Math.abs(screen.value)
 }
-// function to find 10 of any power
-function powerten() {
-    screen.value = Math.pow(10, screen.value)
-    arr.push(screen.value);
-}
+
 // function to find minus_plus
 function minus_plus() {
     screen.value = -(screen.value);
     arr.push(-(screen.value));
 }
-function minusm() {
-    if (arr.length - i <= 0) {
-        i = i + 1;
-        screen.value = 'NO DATA'
-    }
-    else {
-        i = i + 1;
-        screen.value = arr[arr.length - i];
-    }
-
+// function to saveHistory
+let store = undefined
+function saveHistory() {
+    document.getElementsByClassName("disable").disabled = false;
+    store = screen.value;
 }
-
-function plusm() {
-    if (i <= 1) {
-        if (i != 0) {
-            i = i - 1
-        }
-        screen.value = "NO DATA"
-    }
-    else {
-        i = i - 1;
-        screen.value = arr[arr.length - i];
-
-    }
+function showHistory() {
+    if (store == undefined) {
+        screen.value = ""
+    } else
+        screen.value = store
 }
-
-function mcbutton() {
+function minusM() {
+    if (store === undefined) {
+        screen.value = "No Data Found"
+    } else
+        screen.value = eval(screen.value - store);
+}
+function plusM() {
+    if (store === undefined) {
+        screen.value = "No Data Found"
+    } else
+        screen.value = eval(Number(store) + Number(screen.value));
+}
+// Clear Array Data
+function clearHistory() {
     arr = []
-
+    screen.value = "0"
 }
 // function to equals-to
 function result() {
     if (screen.value === ".") {
-        screen.value = "0"
+        screen.value = screen.value
     }
     else {
         screen.value = eval(screen.value);
-        arr.push(screen.value);
+    }
+}
+let count = 1
+function change() {
+    console.log(count)
+    if (count >> 1 == 0) {
+        document.getElementById("change").innerHTML = "1<sup>st</sup>"
+        document.getElementById("square").innerHTML = "x<sup>3</sup>"
+        document.getElementById("sqrt").innerHTML = "&#x221B;x"
+        document.getElementById("expo").innerHTML = "<sup>y</sup>&radic;x"
+        document.getElementById("power10").innerHTML = "2<sup>x</sup>"
+        document.getElementById("log10").innerHTML = "log<sub>y</sub> X"
+        document.getElementById("ln").innerHTML = "e<sup>x</sup>"
+        count = count + 1
+    }
+    else {
+        document.getElementById("change").innerHTML = "2<sup>nd</sup>"
+        document.getElementById("square").innerHTML = "x<sup>2</sup>"
+        document.getElementById("sqrt").innerHTML = "<sup>2</sup>&radic;x"
+        document.getElementById("expo").innerHTML = "x<sup>y</sup>"
+        document.getElementById("power10").innerHTML = "10<sup>x</sup>"
+        document.getElementById("log10").innerHTML = "log"
+        document.getElementById("ln").innerHTML = "ln"
+        count=1
     }
 }
